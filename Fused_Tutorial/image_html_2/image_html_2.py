16933947a97b5ab8ce2c40ce1135400855f8b558
Suryashankar Das <suryashankardas.2002@gmail.com>
2026-03-04 15:58:04 +0530

    Delete Fused_Tutorial directory

--- Fused_Tutorial/image_html_2/image_html_2.py
+++
@@ -1,6 +0,0 @@
-@fused.udf
-def udf(url: str = "https://docs.fused.io/assets/images/dynamic_file_system-7dc205b28ef067029f962754201889c7.png", max_width: int = 800):
-    return f"""<html style="background-color: #1b1f23;">
-<body style="margin:0; display:flex; justify-content:center; align-items:center; height:100vh;">
-    <img src="{url}" style="max-width:{max_width}px; width:100%; height:auto; object-fit:contain;" />
-</body></html>"""
--- Fused_Tutorial/sf_airbnb_listings_2/sf_airbnb_listings_2.py
+++
@@ -1,6 +0,0 @@
-@fused.udf
-def udf(path: str = "s3://fused-sample/demo_data/airbnb_listings_sf.parquet"):
-    import pandas as pd
-    df = pd.read_parquet(path)
-    print(df.T)
-    return df
